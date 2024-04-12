fb80e603c814659a937d34bd4d9b356cdc8dadd5
2318504 <2318504@LTIN487061.cts.com>
2024-04-12 09:43:06 +0530

    updated

--- src/test/java/Factory/BaseClass.java
+++ src/test/java/Factory/BaseClass.java
@@ -93,26 +93,4 @@ public class BaseClass {
 		return logger;
 	}
 	
-	public static String randomeString()
-	{
-		String generatedString=RandomStringUtils.randomAlphabetic(5);
-		return generatedString;
-	}
-	
-	
-	public static String randomeNumber()
-	{
-		String generatedString=RandomStringUtils.randomNumeric(10);
-		return generatedString;
-	}
-	
-		
-	public static String randomAlphaNumeric()
-	{
-	String str=RandomStringUtils.randomAlphabetic(5);
-	 String num=RandomStringUtils.randomNumeric(10);
-	return str+num;
-	}
-	
-	
 }
